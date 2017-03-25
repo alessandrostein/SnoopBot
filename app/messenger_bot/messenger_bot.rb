0c8ff2bb2cbf87d72a60f6f36f8ab8e13b0ac279
alessandrostein <alessandro@alessandrostein.com>
2017-03-25 01:48:13 -0300

    The first users step

--- app/messenger_bot/messenger_bot.rb
+++ app/messenger_bot/messenger_bot.rb
@@ -3,12 +3,48 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
-  message.sender      # => { 'id' => '1008372609250235' }
-  message.seq         # => 73
-  message.sent_at     # => 2016-04-22 21:30:36 +0200
-  message.text        # => 'Hello, bot!'
-  message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
+  if message.quick_reply && message.quick_reply.include?('time')
+    message.reply(text: 'Thank you I will notify you very soon, have a good and productive day!')
+  end
 
-  message.reply(text: 'Ola meu amigão!')
+  if message.quick_reply && message.quick_reply == 'setup_started'
+    User.find_or_create_by(facebook_id: message.sender['id'])
+
+    message.reply(
+        text: 'When should I ask you about your day?',
+        quick_replies: [
+          {
+              content_type: 'text',
+              title: 'Every evening',
+              payload: 'time_morning'
+          },
+          {
+              content_type: 'text',
+              title: 'Every evening',
+              payload: 'time_evening'
+          }
+        ])
+  end
+
+  # message.reply(text: 'Ola meu amigão!')
+end
+
+Bot.on :postback do |postback|
+  case postback.payload
+    when 'Iniciar'
+      postback.reply(
+        text: 'Hello, I am your personal SnoopBot assistant, let me help you with setup procedure',
+        quick_replies: [
+          {
+            content_type: 'text',
+            title: 'I want to set you up',
+            payload: 'setup_started'
+          }
+        ]
+      )
+    when 'Reiniciar'
+      postback.reply(text: 'Reset has been completed')
+    else
+      Rails.logger.warn('Unhandled postback')
+  end
 end
