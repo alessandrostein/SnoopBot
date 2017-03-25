73f099d33c8da463d57f89bff5dc0aef89cb6fe3
alessandrostein <alessandro@alessandrostein.com>
2017-03-25 01:17:46 -0300

    Models user and update

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324141724) do
+ActiveRecord::Schema.define(version: 20170325041406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "updates", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "mood"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "facebook_id"
+    t.datetime "notification_time"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
 end
